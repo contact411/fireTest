d1964bba5cf758ed80ff15a880381c7ddfc2551c
Joel Hampton <joelhampton@gmail.com>
2015-05-07 17:06:30 +1000

    Added firebase configuration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var Firebase = require("firebase");
+var db = new Firebase("https://popping-torch-2236.firebaseio.com/");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
